b1f10e0adad27698bc26ec284ad239b7eb8d7fa4
Raven Wei <125535543+weiraven@users.noreply.github.com>
2026-02-03 16:39:00 -0500

    added constants and basic implementation of player movement and asteroid playing field elements

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -14,7 +16,16 @@ def main():
     dt = 0
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
 
     while True:
         log_state()
@@ -23,11 +34,15 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        screen.fill("black")
-        player.draw(screen)
-        pygame.display.flip()
-
         dt = clock.tick(60) / 1000
+
+        updatable.update(dt)
+
+        screen.fill("black")
+        for sprite in drawable:
+            sprite.draw(screen)
+            
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
