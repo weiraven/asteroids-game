b3e4d33b97daff33a897561fcc332fa9a3452b13
Raven Wei <raven.wei@tiaa.org>
2026-02-04 15:07:56 -0500

    implemented shot object class

---
+++ shot.py
@@ -0,0 +1,13 @@
+import pygame
+from constants import LINE_WIDTH, SHOT_RADIUS
+from circleshape import CircleShape
+
+class Shot(CircleShape):
+    def __init__(self, x, y):
+        super().__init__(x, y, SHOT_RADIUS)
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, "white", self.position, self.radius, LINE_WIDTH)
+
+    def update(self, dt):
+        self.position += self.velocity * dt
